4a8d5f99d646477c3ac220bfe3c3d2c763c87993
Evan Grimmett <evan.grimmett@gmail.com>
2016-05-26 15:48:38 +1000

    added basic view details for details screen. added funky load spinner ;)

--- src/app/organisations/organisation-detail.component.ts
+++ src/app/organisations/organisation-detail.component.ts
@@ -15,6 +15,17 @@ import { OrganisationService } from './organisation.service';
         <label>name: </label>
         <input [(ngModel)]="organisation.Name" placeholder="name"/>
       </div>
+      <ul>
+        <li>Category: {{organisation.Category}}</li>
+        <li>Address: {{organisation.StreetAddress}}<br/>{{organisation.Suburb}} {{organisation.Postcode}}<br/>{{organisation.State}}</li>
+        <li>Email: {{organisation.Email}}</li>
+        <li>Phone: {{organisation.Phone}}</li>
+        <li>PhoneE164: {{organisation.PhoneAsE164}}</li>
+        <li>Fax: {{organisation.Fax}}</li>
+        <li>Website: {{organisation.Website}}</li>
+        <li>Lat/Lng: {{organisation.Lat}},{{organisation.Lng}}</li>
+        <li>Distance: {{organisation.Distance}}</li>
+      </ul>
     </div>
   `
 })
