b61b8ef1c3e70c3b27991e32d83253c0a80a85d8
Daniel H (P) <daniel.hoek@dss.gov.au>
2016-05-31 13:33:24 +1000

    Markers now appear on map based on search results

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,54 +1,66 @@
+/*
+
+This map component provides the Google Map UI 
+with the markers of the organisations returned by the search results
+
+This component works as follows:
+- Defines an array of markers, initially empty
+- Subscribes to the OrganisationService for any updates to the Organisations Search Results
+- If the results change then update the list of markers
+
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
+
+import { OrganisationService } from '../organisations/organisation.service';
+import { Organisation } from '../organisations/organisation';
 
 import { Marker } from './marker'
 
 @Component({
   moduleId: module.id,
-  selector: 'mapComponent',
+  selector: 'map-component',
   templateUrl: 'map.component.html',
   styleUrls: ['map.component.css'],
   directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES]
 })
 
 export class MapComponent implements OnInit {
-  lat: number = 50;
-  lng: number = 8;
+  // default lat/long focus of the map
+  lat: number = -33.7711375;
+  lng: number = 151.0802965;
 
-  markers: Marker[] =
-    [{
-      lat: 50,
-      lng: 8,
-      label: 'A'
-    }];
+  // Create an array of Markers and initiate as empty
+  markers: Marker[] = [];
 
-  testMarker: Marker = 
-    {
-      lat: 50.001,
-      lng: 8.001,
-      label: 'B'
-    };
-
-
-  constructor() {}
+  constructor(
+    private organisationService: OrganisationService
+    ) {}
 
   // When the component starts
   ngOnInit () {
+    this.subscribeToOrganisationListUpdates();
   }
 
-  updateLatLong (lat: number, lng: number) {
-
+  subscribeToOrganisationListUpdates() {
+    this.organisationService.orgListSource$
+    .subscribe(
+      organisations => this.clearAndAddNewMarkers(organisations),
+      error =>  console.log(error));
   }
-  
-  addTestMarker(){
-    console.log("button pushed");
-    
-    this.markers.push(    
-      {
-      lat: 50.001,
-      lng: 8.001,
-      label: 'B'
-    });
-    console.log(this.markers);
+
+  clearAndAddNewMarkers(organisations: Organisation[]) {
+    // Clear the markers Array
+    this.markers = [];
+
+    // For each search result, create a new marker
+    organisations.forEach(organisation => {
+      this.markers.push({
+        lat: Number(organisation.Lat), 
+        lng: Number(organisation.Lng), 
+        label: 'O'})
+      }
+    );
   }
 }
