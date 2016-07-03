a795241e83d9201ee94ccd219ffcd8afdd71861e
Evan Grimmett <evan.grimmett@dss.gov.au>
2016-07-04 09:58:56 +1000

    updated packages, sadly  -prod build not working still

--- config/environment.prod.ts
+++ config/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapsAPIkey: 'AIzaSyB17zG-jByFLcX1kyG5-pBch6MwM0RDxxY', // NDAP Dev API key mobile.apps@dss.gov.au,
+  apiHost: "https://finder.dss.gov.au/"
+
 };
