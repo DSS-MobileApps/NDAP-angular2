66276226e4e021b57f00199b3630b75ad7a1f94d
Evan Grimmett <evan.grimmett@dss.gov.au>
2016-10-07 17:12:48 +1100

    set SearchAreaComponent as root components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -48,7 +48,8 @@ export const routes: Routes = [
   // Root Url
   {
   path: '',
-  component: OrganisationsComponent
+  // component: OrganisationsComponent
+  component: SearchAreaComponent
 },
 // {
 //   path: '',
